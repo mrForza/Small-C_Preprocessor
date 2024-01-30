f9674be6adb7b2c105ceda4cab6451ebcbd93dcf
Roman Gromov <gromov1921@bk.ru>
2024-01-30 13:44:44 +0300

    add: file_handler module; change: file's validators

--- include/file_handler/file_handler.c
+++ include/file_handler/file_handler.c
@@ -1,1 +1,57 @@
 #include "file_handler.h"
+#include "file_validator.h"
+#include <stdlib.h>
+
+#define MAX_FILENAME_LENGTH 256
+
+const char* GENERAL_FILE_ERROR = "You have input the incorrect filename";
+
+
+char* get_full_name() {
+    char* file_name = (char*)malloc(MAX_FILENAME_LENGTH);
+    char* result_line = file_name;
+    while (true) {
+        printf("\n\nEnter the name of your source file: ");
+        for (int i = 0; i < MAX_FILENAME_LENGTH; ++i) {
+            int ascii_code = fgetc(stdin);
+            if (ascii_code == EOF || ascii_code == '\n') {
+                break;
+            }
+            file_name[i] = (char)ascii_code;
+        }
+        return result_line;
+    }
+}
+
+
+char** split_full_name(const char* full_name) {
+    int dot_index = 0;
+    for (int i = 0; i < sizeof(full_name); ++i) {
+        if (full_name[i] == '.') {
+            dot_index = i;
+            break;
+        }
+    }
+    char* name = (char*)malloc(dot_index + 1); // +1 for \0 symbol in the end of name
+    char* extension = (char*)malloc(sizeof(full_name) - dot_index + 1);
+    int j = 0;
+    for (int i = 0; i < sizeof(full_name); ++i) {
+        if (i < dot_index) {
+            name[i] = full_name[i];
+        } else if (i > dot_index) {
+            extension[j] = full_name[i];
+            ++j;
+        } else {
+            name[dot_index] = '\0';
+        }
+
+        if (i == sizeof(full_name) - 1) {
+            extension[j + 1] = '\0';
+        }
+    }
+
+    char** array = (char**)malloc(sizeof(name) + sizeof(extension));
+    array[0] = name;
+    array[1] = extension;
+    return array;
+}
